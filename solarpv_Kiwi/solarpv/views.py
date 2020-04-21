37b6fad45535f682f03d55e90cec2cbead4a25c8
Torey <ttakaha1@asu.edu>
2020-04-20 18:22:30 -1000

    Create template and static, add html, css and pictures. Altered url.py and views.py. Website is up and running.

--- solarpv_Kiwi/solarpv/views.py
+++ solarpv_Kiwi/solarpv/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'solarpv/homepage.html')
+
+def login(request):
+    return render(request, 'solarpv/login.html')
+
+def registration(request):
+    return render(request, 'solarpv/registration.html')
